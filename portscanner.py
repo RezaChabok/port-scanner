07c930dd312ed6de75d4cd85c3213b571789ee4a
Reza Chabok <64315366+RezaChabok@users.noreply.github.com>
2022-09-18 00:07:02 +0430

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,27 @@
+import socket
+import termcolor
+import ipaddress
+
+
+def scan(target, ports):
+	print("\n" + termcolor.colored((f"-> Starting Scan For {target}:"), "red"))
+	for port in range (1, ports):
+		try:
+			sock = socket.socket()
+			sock.settimeout(3)
+			sock.connect((target, port))
+			print(termcolor.colored((f"[+] Port Opened {port} -> "), "cyan"), socket.getservbyport(port))
+		except TimeoutError as e :
+			print("\n" + termcolor.colored((f"IP {target} is Not Available!!!"), "red"))
+			break
+		except : pass
+
+
+targets = input(termcolor.colored(("[*] Enter Target To Scan(split them by ,): "), "blue"))
+ports = int(input(termcolor.colored(("[*] Enter How Many Ports You Want To Scan: "), "blue")))
+if ',' in targets:
+	print(termcolor.colored(("[*] Scanning Multiple Targets") ,"green"))
+	for ip_addr in targets.split(",") :
+		scan(ip_addr.rstrip(' '), ports)
+
+else : scan(targets ,ports)
