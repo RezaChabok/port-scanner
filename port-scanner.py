19c2c839b33edafc79e30697fb9ae317fb3e1011
Reza Chabok <mail-to-bad-sector@proton.me>
2023-09-24 16:57:14 +0330

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -1,8 +1,6 @@
 import socket
 import termcolor
 
-
-
 def scan(target, ports):
 	print("\n" + termcolor.colored((f"-> Starting Scan For {target}:"), "green"))
 	for port in range (1, ports):
@@ -15,7 +13,8 @@ def scan(target, ports):
 			print("\n" + termcolor.colored((f"IP {target} is Not Available!!!"), "red"))
 			break
 		except : pass
-def checker(ip):
+			
+def check(ip):
 	for num in ip.split('.'):
 		if len(num) > 1 :
 			if int(num) >= 250:
@@ -26,26 +25,28 @@ def checker(ip):
 	except socket.error:
 		return False
 	return True
-def finder(ip, flag):
-		count = 0
-		loc = 0
-		if flag == 8:
-			end = 1
-		else : end = 2
-		while True:
-			if count == end:
- 				break
-			elif ip.split("/")[0][loc] == ".":
-				count +=1
-			loc += 1
-		return loc-1	
+	
+def find(ip, flag):
+	count = 0
+	loc = 0
+	if flag == 8:
+		end = 1
+	else : end = 2
+	while True:
+		if count == end:
+			break
+		elif ip.split("/")[0][loc] == ".":
+			count +=1
+		loc += 1
+	return loc-1	
+	
 while True:
 	targets = input(termcolor.colored(("[*] Enter Target To Scan(split them by ,): "), "blue"))
 	ports = int(input(termcolor.colored(("[*] Enter How Many Ports You Want To Scan: "), "blue")))
 	if ',' in targets:
 		print(termcolor.colored(("[*] Scanning Multiple Targets") ,"green"))
 		for ip_addr in targets.split(",") :
-			if checker(ip_addr.split("/")[0]):	
+			if check(ip_addr.split("/")[0]):	
 				if '/' in ip_addr:
 					if ip_addr.split("/")[1] == '32':
 						scan(ip_addr.split("/")[0], ports)
@@ -62,12 +63,12 @@ while True:
 								break
 							scan(ip_addr.split("/")[0][:last]+str(i), ports)
 					elif ip_addr.split("/")[1] == '16':
-						loc = finder((ip_addr.split("/")[0]), 16)
+						loc = find((ip_addr.split("/")[0]), 16)
 						for i in range(1,250):
 							for j in range(1, 250):
 								scan((ip_addr[:loc]+'.'+str(i)+'.'+str(j)), ports)
 					elif ip_addr.split("/")[1] == '8':
-						loc = finder((ip_addr.split("/")[0]), 8)
+						loc = find((ip_addr.split("/")[0]), 8)
 						for i in range(1, 250):
 							for j in range(1,250):	
 								for k in range(1, 250):
@@ -79,8 +80,7 @@ while True:
 			else:
 				print(termcolor.colored("IP is not Valid!", "red"))
 
-	else : 
-		if checker(targets.split("/")[0]):	
+	elif check(targets.split("/")[0]):	
 				if '/' in targets:
 					if targets.split("/")[1] == '32':
 						scan(targets.split("/")[0], ports)
@@ -97,19 +97,19 @@ while True:
 								break
 							scan(targets.split("/")[0][:last]+str(i), ports)
 					elif targets.split("/")[1] == '16':
-						loc = finder((targets.split("/")[0]), 16)
+						loc = find((targets.split("/")[0]), 16)
 						for i in range(1,250):	
 							for j in range(1, 250):
 								scan((targets[:loc]+'.'+str(i)+'.'+str(j)), ports)
 					elif targets.split("/")[1] == '8':
-						loc = finder((targets.split("/")[0]), 8)
+						loc = find((targets.split("/")[0]), 8)
 						for i in range(1, 250):
 							for j in range(1, 250):
 								for k in range(1, 250):
 									scan((targets[:loc]+'.'+str(i)+'.'+str(j)+'.'+str(k)), ports)
 					else :print(termcolor.colored(f"IP {targets} is have a problem (just support ip/32 , ip/24 and ip/8)", "red"))
 				else:scan(targets.rstrip(' '), ports)
-		else : print(termcolor.colored("IP is not valid !!!", "red"))
+	print(termcolor.colored("IP is not valid !!!", "red"))
 	choose = input("Do you Want Try Again?!(y/n) > ")
 	if choose.lower() == 'n':
 		break	
